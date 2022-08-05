256a27e975eb11417792288bddddb27d90cd7166
joaquinquiroga22 <joaquinquiroga22@gmail>
2022-08-05 15:38:58 -0300

    first commit

---
+++ Ejercicio.js
@@ -0,0 +1,35 @@
+function esNatural(num) {
+    // Devuelve true si el num es Natural
+    // Usa Recursividad
+}
+
+function Colores(colores) {
+    // Aquellos colores negros cambiar su palabra a Black
+    // Devuelve el arreglo
+}
+
+var colores = ['Blanco', 'Negros', 'Rojo', 'Amarillo', 'Negros', 'Verde']
+
+function Filtrado(colores) {
+    // Del resultado de la funcion Colores() filtra aquellos colores llamados Black
+    // ejemplo ---> ['Blanco', 'Rojo', 'Amarillo', 'Verde']
+}
+
+function Filtrar(personas){
+    // Filtrar aquellas personas que sean menores de edad, siendo mayor de edad a partir de los 18 años
+    // Mostrar arreglo
+}
+
+const personas = [
+
+    { nombre: 'Carlos', edad: 20},
+    { nombre: 'Esteban', edad: 15},
+    { nombre: 'Franco', edad: 12},
+    { nombre: 'Estela', edad: 19},
+    { nombre: 'Juan', edad: 10},
+    { nombre: 'Mirtha', edad: 17}
+];
+
+
+
+
